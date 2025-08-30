8865fcd4c60dae8361396f33a2d5b2d7e0080a3a
Cadenjacks <cjackson3@academic.rrc.ca>
2025-08-30 15:43:57 -0500

    Update Transit_App.py
    Adding the functionality to have the user be able to select a specific stop and see more info

--- Transit_App.py
+++ Transit_App.py
@@ -24,4 +24,43 @@ for i, stop in enumerate(stops, start=1):
     stop_name = stop['name']
     street = stop['street']['name']
     print(f"{i}. Stop #{stop_num} - {stop_name} at {street}")
+
+# Let user pick a stop
+choice = input("\nEnter the stop number you want to check: ")
+selected_stop = next((s for s in stops if str(s['number']) == choice), None)
+
+# If user enters invalid stop
+if not selected_stop:
+    print("Invalid stop number.")
+    exit()
+
+stop_num = selected_stop['number']
+
+# Get arrival times for selected stop
+url_schedule = f"https://api.winnipegtransit.com/v3/stops/{stop_num}/schedule.json?api-key={API_KEY}"
+schedule_data = get(url_schedule).json()
+
+stop_schedule = schedule_data.get('stop-schedule', {})
+route_schedules = stop_schedule.get('route-schedules', {})
+
+if not route_schedules:
+    print("No upcoming buses for this stop.")
+    exit()
+
+# Display upcoming stops
+print(f"\nUpcoming Arrivals at stop #{stop_num}:\n")
+
+for route_schedule in route_schedules:
+    route = route_schedule.get('route', {})
+    route_name = route.get('name', 'Unknown Route')
+    scheduled_stops = route_schedule.get('scheduled-stops', [])
+
+    print(f"Route {route.get('key')} - {route_name}")
+    for scheduled in scheduled_stops[:3]: # Only display the next 3 arrivals
+        times = scheduled.get('times', {})
+        scheduled_time_str = times.get('arrival', {}).get('scheduled')
+        estimated_time_str = times.get('arrival', {}).get('estimated')
+
+        print(f"Scheduled: {scheduled_time_str}, Estimated: {estimated_time_str}")
     
+    print()
